fc64a78990a5ea639d1255e2f121e6776e6c092b
Sandra <Sandra@DESKTOP-22AD1I6>
2020-02-02 16:55:10 -0800

    Imprimir un mensaje..

--- AprendiendoGit/src/aprendiendogit/AprendiendoGit.java
+++ AprendiendoGit/src/aprendiendogit/AprendiendoGit.java
@@ -15,7 +15,7 @@ public class AprendiendoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+                System.out.println("Hola, aprendiendo git.........");
     }
     
 }
